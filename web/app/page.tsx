2309a75297a3b83670c06b9607f74eda117b7267
Cristian Șerb <mail@criss.dev>
2023-11-04 22:49:20 +0000

    adding model

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -6,8 +6,27 @@ import { ethers } from 'ethers'
 import { EthersAdapter } from '@safe-global/protocol-kit'
 import Safe from '@safe-global/protocol-kit'
 import Image from 'next/image'
+import Button from '@mui/material/Button';
+import Modal from '@mui/material/Modal';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import TextField from '@mui/material/TextField';
 
 import axios from 'axios';
+
+import { IExecWeb3mail } from "@iexec/web3mail"
+
+
+// const sendMail = async (emailContent, protectedData) => {
+// 	const emailSubject = 'You have received an message from a potential acquirer - Nata'
+// 	const result = await web3mail.sendEmail({
+// 		protectedData,
+// 		emailSubject,
+// 		emailContent
+// 	})
+//
+// 	console.log(result)
+// }
 
 const getBalanceTokens = async (address: string) => {
   try {
@@ -30,7 +49,24 @@ const getBalanceTokens = async (address: string) => {
 export default function Home() {
   const [balance, setBalance] = React.useState("0");
   const [tokens, setTokens] = React.useState<any>();
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => {
+    // TODO: send email
+    setOpen(false)
+  }
   const safeAddress = '0xAecDFD3A19f777F0c03e6bf99AAfB59937d6467b'
+  const style = {
+    position: 'absolute' as 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    backgroundColor: 'gray',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+  };
 
   useEffect(() => {
     async function fetchData() {
@@ -46,6 +82,9 @@ export default function Home() {
 
       const tokens = await getBalanceTokens(safeAddress);
       setTokens(tokens);
+
+      // const iexProvider = new ethers.providers.Web3Provider(window.ethereum)
+      // const web3mail = new IExecWeb3mail(iexProvider)
     }
 
     fetchData();
@@ -75,6 +114,24 @@ export default function Home() {
             )
           })}
           </p>
+          <Button variant="contained" onClick={handleOpen} sx={{backgroundColor: 'orange', float: 'right'}} >Contact</Button>
+          <Modal
+            open={open}
+            onClose={handleClose}
+            aria-labelledby="modal-modal-title"
+            aria-describedby="modal-modal-description"
+            sx={{borderRadius: '10px'}}
+          >
+            <Box sx={style}>
+              <div style={{display: 'grid', flexDirection: 'row' }}>
+              <TextField
+                id="outlined-basic" label="Contact DAO privately" variant="outlined"
+                sx={{marginBottom: '16px'}}
+              />
+              <Button variant="contained" onClick={handleClose}>Send</Button>
+              </div>
+            </Box>
+          </Modal>
         </div>
       </main>
     </>
